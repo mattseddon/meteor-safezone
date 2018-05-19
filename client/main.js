25c26057fc8c1e5286b20663ff61b477c37db03c
Matt Seddon <mattseddon@hotmail.com>
2018-05-19 13:43:16 +1000

    navbar added successfully

--- client/main.js
+++ client/main.js
@@ -1,9 +1,13 @@
+
 import '../imports/ui/pages/athleteProfile.js';
 import '../imports/startup/accounts-config.js';
+import '../imports/ui/components/custNavBar.js';
+import './main.html';
+
 //import '../imports/ui/components/login.js'
 
-import '../imports/ui/pages/homePage.html'
-import '../imports/ui/pages/athleteProfile.html'
-import '../imports/ui/components/login.html'
-import '../imports/ui/components/signup.html'
-import '../imports/ui/components/header.html'
+import '../imports/ui/pages/homePage.html';
+import '../imports/ui/pages/athleteProfile.html';
+import '../imports/ui/components/login.html';
+import '../imports/ui/components/signup.html';
+import '../imports/ui/components/header.html';
---
+++ imports/ui/components/custNavBar.js
@@ -0,0 +1,11 @@
+import './custNavBar.html';
+// import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
+
+// Template.custNavBar.onCreated(function() {
+//     // Subscribe only the relevant subscription to this page
+//     var self = this;
+//     self.autorun(function() { // Stops all current subscriptions
+//         var id = FlowRouter.getParam('id'); // Get the collection id from the route parameter
+//         // self.subscribe('singlePost', id); // Subscribe to the single entry in the collection with the route params id
+//     });
+// });
